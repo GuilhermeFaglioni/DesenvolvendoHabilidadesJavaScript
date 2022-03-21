5e88cc98c482ab9079b195af2c76188099ae8430
GuilhermeFaglioni <98924709+GuilhermeFaglioni@users.noreply.github.com>
2022-03-21 15:23:11 -0300

    Calculadora.js
    This is a console sum calculator.
    Isso é uma calculadora de soma no console.

---
+++ calculadora.js
@@ -0,0 +1,7 @@
+//Calculadora de adição dentro do console
+
+alert("Digite os números os quais você deseja que sejam somados"); 
+var x = prompt("Primeiro número:"); //prompt para receber a primeira variável
+var y = prompt("Segundo número:"); //prompt para receber a segunda variável
+var z = Number(x) + Number(y); //Cálculo matemático de soma e conversão dos prompts de String para Int
+alert("A soma é: "+ z); //Alert com o resultado
