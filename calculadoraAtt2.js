9a17201672afbea67b92d9a2a1e7f582d0d3f551
Guilherme Faglioni <98924709+GuilhermeFaglioni@users.noreply.github.com>
2022-03-21 18:25:57 -0300

    CalculadoraAtt2.js
    This sum calculator now is in a loop that ends with a "N" input on any of the variables
    Agora a calculadora de soma está em um loop que termina quando "N" for inputado em qualquer uma das variáveis

---
+++ calculadoraAtt2.js
@@ -0,0 +1,17 @@
+//Calculadora de adição dentro do console com verificação se o valor é maior que 10
+while(true){
+alert("Digite os números os quais você deseja que sejam somados"); 
+var x = prompt("Primeiro número:"); //prompt para receber a primeira variável
+var y = prompt("Segundo número:"); //prompt para receber a segunda variável
+var z = Number(x) + Number(y); //Cálculo matemático de soma e conversão dos prompts de String para Int
+if(x == 'N' || y == 'N') {
+    alert("você parou a calculadora");
+    break;
+}
+alert("A soma é: "+ z); //Alert com o resultado
+if(z > 10) {
+    alert("A sua soma é maior que 10");
+} else {
+    alert("A sua soma é menor que 10");
+};
+};
