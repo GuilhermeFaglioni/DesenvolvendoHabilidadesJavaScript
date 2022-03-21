c4c2a0550b13299611746cdbf70f5ddebd564409
Guilherme Faglioni <98924709+GuilhermeFaglioni@users.noreply.github.com>
2022-03-21 18:47:30 -0300

    CalculadoraAtt3.js
    Going back to the basic sum calculator, but now working by functions
    De volta a calculadora de soma básica, mas agora funcionado por funções

---
+++ calculadoraAtt3.js
@@ -0,0 +1,11 @@
+// Calculadora de soma feita por função
+
+function soma(x, y){ // função de soma
+    var res = Number(x) + Number(y);
+    alert("Resultado: "+res)
+}
+
+var x = prompt("Digite o primeiro valor:");
+var y = prompt("Digite o segundo valor:"); 
+
+soma(x, y);
