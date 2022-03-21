4eeca332912fba6e52f21e3f752e60aa6e22b3e4
Guilherme Faglioni <98924709+GuilhermeFaglioni@users.noreply.github.com>
2022-03-21 16:11:10 -0300

    CalculadoraAtt1.js

--- calculadoraAtt1.js
+++ calculadoraAtt1.js
@@ -1,4 +1,4 @@
-//Calculadora de adição dentro do console com verificação se o valor é maior que 10
+//Calculadora de adição dentro do console com verificação se o valor é maior que 10.
 
 alert("Digite os números os quais você deseja que sejam somados"); 
 var x = prompt("Primeiro número:"); //prompt para receber a primeira variável
