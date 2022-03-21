708cbc5df32d70599ae5a9f6a46d05b44b07873d
Guilherme Faglioni <98924709+GuilhermeFaglioni@users.noreply.github.com>
2022-03-21 18:27:55 -0300

    CalculadoraAtt2.js
    Now the sum calculator works in loop that stops with the "N" input in any of the variables
    Agora a calculadora funciona em loop que termina quando o "N" é inputado em qualquer uma das variáveis

--- calculadoraAtt2.js
+++ calculadoraAtt2.js
@@ -1,10 +1,10 @@
-//Calculadora de adição dentro do console com verificação se o valor é maior que 10
-while(true){
+//Calculadora de adição dentro do console com verificação se o valor é maior que 10 e funciona em loop terminado com input "N" em qualquer uma das variáveis
+while(true){ // Início do loop
 alert("Digite os números os quais você deseja que sejam somados"); 
 var x = prompt("Primeiro número:"); //prompt para receber a primeira variável
 var y = prompt("Segundo número:"); //prompt para receber a segunda variável
 var z = Number(x) + Number(y); //Cálculo matemático de soma e conversão dos prompts de String para Int
-if(x == 'N' || y == 'N') {
+if(x == 'N' || y == 'N') { // Condição para parar o loop
     alert("você parou a calculadora");
     break;
 }
